4cfe27e3be80a0a988c638d885302afd6813d12b
Srinath Anand <srinath.anand@gmail.com>
2021-03-31 20:36:50 -0400

    modify UI element for priests

--- src/components/Priests.js
+++ src/components/Priests.js
@@ -2,13 +2,12 @@ import React from "react";
 
 const Priests = () => {
   return (
-    <div class="card">
-      <div class="card-body">
+    <div class="priests">
+      <h1>Priests</h1>
       <h4>Sri. BALAKRISHNA SASTRY</h4>
         <p>+1 603 598 4864</p>
         <p>Available in the premises between 7 am EST to noon EST and 5 pm EST to 9 pm EST</p>
       </div>
-    </div>
   );
 };
 
